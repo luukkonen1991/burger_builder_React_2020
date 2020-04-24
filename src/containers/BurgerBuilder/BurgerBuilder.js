6a47ef540c91a6bee56b59ca06a08cb7db18f23b
luukkonen1991 <luukkonen1991@gmail.com>
2020-04-24 15:28:28 +0300

    ADDED price to buidcontrols which is displayed in two decimal accuracity

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -79,6 +79,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disableInfo}
+          price={this.state.totalPrice}
         />
       </Auxiliary>
     );
