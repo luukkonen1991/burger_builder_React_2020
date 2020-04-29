a1f09977e950665c5298c54df4453a1a38f0a9bd
luukkonen1991 <luukkonen1991@gmail.com>
2020-04-29 19:32:53 +0300

    ADDED more functionlity to sideDrawer mobileview

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => {
-  return (
-    <Auxiliary>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes.Content} >
-        {props.children}
-      </main>
-    </Auxiliary>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  SideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler} />
+        <main className={classes.Content} >
+          {this.props.children}
+        </main>
+      </Auxiliary>
+    );
+  }
+}
 
 export default Layout;
 
