bb56570b27909bf9a56d976524038a916b30c84a
luukkonen1991 <luukkonen1991@gmail.com>
2020-05-08 17:59:57 +0300

    handling overall form validation and fixed some errors

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,9 +85,12 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
-        value: ''
+        value: '',
+        validation: {},
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   };
 
@@ -146,8 +149,16 @@ class ContactData extends Component {
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     console.log(updatedFormElement);
+
+    let formIsValid = true;
+
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
     this.setState({
-      orderForm: updatedOrderForm
+      orderForm: updatedOrderForm,
+      formIsValid: formIsValid
     });
   };
 
@@ -174,7 +185,9 @@ class ContactData extends Component {
             shouldValidate={formElement.config.validation}
             touched={formElement.config.touched} />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button
+          btnType="Success"
+          disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
     if (this.state.loading) {
