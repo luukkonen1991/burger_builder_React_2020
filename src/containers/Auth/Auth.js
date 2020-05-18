5b18c001828437de102af8888e29e5b3acf8a024
luukkonen1991 <luukkonen1991@gmail.com>
2020-05-18 20:01:56 +0300

    ADDING sign-in

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -38,7 +38,8 @@ class Auth extends Component {
         valid: false,
         touched: false
       }
-    }
+    },
+    isSignup: true
   };
 
   checkValidity(value, rules) {
@@ -80,7 +81,15 @@ class Auth extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+  };
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {
+        isSignup: !prevState.isSignup
+      };
+    });
   };
 
   render() {
@@ -114,6 +123,9 @@ class Auth extends Component {
             Submit
           </Button>
         </form>
+        <Button
+          clicked={this.switchAuthModeHandler}
+          btnType='Danger' >SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
       </div>
     );
   }
@@ -121,7 +133,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
   };
 };
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,7 +23,7 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
@@ -31,8 +31,13 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true
     };
-    axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKeys.FIREBASE_API_KEY}
-    `, authData)
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKeys.FIREBASE_API_KEY}
+    `;
+    if (!isSignup) {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKeys.FIREBASE_API_KEY}
+      `;
+    }
+    axios.post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data));
