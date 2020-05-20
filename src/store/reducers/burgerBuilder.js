252147d01314588be352f913f3cf15ac5cbbecb0
luukkonen1991 <luukkonen1991@gmail.com>
2020-05-20 06:56:44 +0300

    using updateObject int the entire app

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../utilities/utility';
+import { updateObject } from '../../shared/utility';
 
 const initialState = {
   ingredients: null,
