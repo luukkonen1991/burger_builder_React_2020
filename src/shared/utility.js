8701b61393cc8579e91705ee411015ef1b097bf9
luukkonen1991 <luukkonen1991@gmail.com>
2020-05-20 07:12:53 +0300

    redux only on devmode

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -3,4 +3,26 @@ export const updateObject = (oldObject, updatedProperties) => {
     ...oldObject,
     ...updatedProperties
   };
-}
+};
+
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+
+  if (!rules) {
+    return true;
+  }
+
+  if (rules.required) {
+    isValid = value.trim() !== '' && isValid;
+  }
+
+  if (rules.minLength) {
+    isValid = value.length >= rules.minLength && isValid;
+  }
+
+  if (rules.maxLenght) {
+    isValid = value.length <= rules.maxLenght && isValid;
+  }
+
+  return isValid;
+};
