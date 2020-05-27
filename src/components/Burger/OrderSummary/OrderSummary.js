ed6dd74ff136768d207f46b2b547f7f33815ae12
luukkonen1991 <luukkonen1991@gmail.com>
2020-05-27 17:57:27 +0300

    react.memo on modal component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, { } from 'react';
 
 import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
+const OrderSummary = props => {
   // This cloud be a functional component => doesent have to be class
-  componentDidUpdate() {
-    // console.log('componentDidUpdate');
-  }
 
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients)
-      .map(igKey => {
-        return (
-          <li key={igKey} >
-            <span style={{ textTransform: 'capitalize' }} >{igKey}</span>: {this.props.ingredients[igKey]}
-          </li>);
-      });
 
-    return (
-      <Auxiliary>
-        <h3>Your Order</h3>
-        <p>Ingredients:</p>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button
-          btnType='Danger'
-          clicked={this.props.buyCancelled} >
-          CANCEL
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return (
+        <li key={igKey} >
+          <span style={{ textTransform: 'capitalize' }} >{igKey}</span>: {props.ingredients[igKey]}
+        </li>);
+    });
+
+  return (
+    <Auxiliary>
+      <h3>Your Order</h3>
+      <p>Ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button
+        btnType='Danger'
+        clicked={props.buyCancelled} >
+        CANCEL
       </Button>
-        <Button
-          btnType='Success'
-          clicked={this.props.buyContinued} >
-          CONTINUE
+      <Button
+        btnType='Success'
+        clicked={props.buyContinued} >
+        CONTINUE
       </Button>
-      </Auxiliary>
-    );
-  }
-}
+    </Auxiliary>
+  );
+};
 
 export default OrderSummary;
