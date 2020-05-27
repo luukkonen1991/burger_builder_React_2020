f24d6ec3c7d1752177afeeda1de627575172a96d
luukkonen1991 <luukkonen1991@gmail.com>
2020-05-27 17:49:17 +0300

    more hooks and component/container refactoring

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import * as actions from '../../../store/actions/index';
 
-class Logout extends Component {
+const Logout = props => {
+  const { onLogout } = props;
 
-  componentDidMount() {
-    this.props.onLogout();
-  }
+  useEffect(() => {
+    onLogout();
+  }, [onLogout]);
 
-  render() {
-    return <Redirect to="/" />;
-  }
-}
+  return <Redirect to="/" />;
+
+};
 
 const mapDispatchToProps = dispatch => {
   return {
